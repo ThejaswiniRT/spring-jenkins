9faf9611b3d2158cc1dac58debda28029e96a1ac
ThejaswiniRT <thejurt.gowda@gmail.com>
2021-08-26 18:15:13 +0530

    spring-jenkins

---
+++ src/main/java/org/theju/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,29 @@
+package org.theju.springjenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+  public  static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+    public static void main(String[] args) {
+        logger.info("Application Executed");
+        SpringApplication.run(SpringJenkinsApplication.class, args);
+
+    }
+
+
+
+    @PostConstruct
+    public void init()
+    {
+        logger.info("init method executed");
+    }
+}
---
+++ src/test/java/org/theju/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,22 @@
+package org.theju.springjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+    @Test
+   public  void contextLoads() {
+logger.info("executing test");
+
+       assertEquals(true,true);
+    }
+
+}
